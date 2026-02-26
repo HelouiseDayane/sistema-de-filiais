63b2cec6ecdff6f1b71bc987b973ec78b598c743
HelouiseDayane <helouisedayane@gmail.com>
2026-02-26 16:49:12 +0000

    Remove arquivos sensíveis do versionamento

--- brunocakes_front_v3/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  VITE_API_BASE_URL: string;
-  VITE_DOMAIN_BASE_URL: string;
-  // adicione outras variáveis que você usa
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
